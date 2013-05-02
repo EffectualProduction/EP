dde6e191e98fb309b9fc3cc58ccb273d215e5abc
“Cristian_Jimenez” <adsi@ADSI.(none)>
2013-05-02 09:41:15 -0500

    Adicion de Lazybox para la vista de 'Solicitudes'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require lazybox
 //= require_tree .
+
+
+$(document).ready(function() {
+ 
+ $('a#show_team_link').lazybox({closeImg: true});
+ 
+ $('a[rel*=lazybox]').lazybox({close: false, modal: false, opacity: 0.7, klass: 'lazybox', speed: 200});
+ $.lazybox.settings = {cancelClass: "button green", submitClass: 'button red', closeImg: false, niceClose: false}
+ $.rails.allowAction = $.lazybox.confirm;
+
+});
+$(document).ready(function() {
+ 
+ $('a#show_team_link').lazybox({closeImg: true});
+ 
+ $('a[rel*=lazybox]').lazybox({close: false, modal: false, opacity: 0.7, klass: 'lazybox', speed: 200});
+ $.lazybox.settings = {cancelClass: "button green", submitClass: 'button red', closeImg: false, niceClose: false}
+ $.rails.allowAction = $.lazybox.confirm;
+
+});
